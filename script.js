8ae4eecd421a1e3fd95ca8816fe4cbb014e2fc44
JuliannaRkg544 <juliana.rkg544@gmail.com>
2022-04-14 10:58:17 -0300

    trabalhando na barra lateral de pedidos

---
+++ script.js
@@ -0,0 +1,7 @@
+function hide(){
+    document.querySelector(".pizza-box").classList.add("hidden");
+    document.querySelector("aside").classList.add("hidden")
+    document.querySelector(".confirmation-box").classList.add("hidden")
+}
+
+hide()
